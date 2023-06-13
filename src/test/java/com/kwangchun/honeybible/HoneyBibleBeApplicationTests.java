3d6946df356ba0954c84cc44e777134d1fbe6e42
ben.ny <ben.ny@kakaocorp.com>
2023-06-13 09:54:24 +0900

    feature : oracle DB 에 select 예시 코드 추가

---
+++ src/test/java/com/kwangchun/honeybible/HoneyBibleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwangchun.honeybible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyBibleBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
