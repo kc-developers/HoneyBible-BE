065a9e0f11a848872182d857559dbda002fb42ad
ben.ny <ben.ny@kakaocorp.com>
2023-10-20 21:12:19 +0900

    feat : Readcheck 의 gson 위치 수정

--- src/main/java/com/kwangchun/honeybible/Service/ReadcheckService.java
+++ src/main/java/com/kwangchun/honeybible/Service/ReadcheckService.java
@@ -1,8 +1,14 @@
 package com.kwangchun.honeybible.Service;
 
+import com.google.gson.Gson;
 import com.kwangchun.honeybible.Repository.ReadcheckRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -10,8 +16,21 @@ public class ReadcheckService {
 
     private final ReadcheckRepository readcheckRepository;
 
+    private final Gson gson;
+
     public String getReadchecks() {
-        return readcheckRepository.selectAll();
+        return gson.toJson(readcheckRepository.selectAll());
+    }
+    
+    public String getReadcheck(String memberNum, String dateKey) {
+        boolean read = true;
+        try {
+            readcheckRepository.selectOne(dateKey, memberNum);
+        } catch (EmptyResultDataAccessException e) {
+            read = false;
+        }
+
+        return gson.toJson(read);
     }
     
     public String getReadcheck(String dateKey) {
