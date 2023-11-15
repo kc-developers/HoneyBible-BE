175d638f2b1ae87c6cc191cb37ccad54d720af93
ben.ny <ben.ny@kakaocorp.com>
2023-11-15 09:18:56 +0900

    feat : login request 변수에 기본 값 설정

--- src/main/java/com/kwangchun/honeybible/dto/LoginRequest.java
+++ src/main/java/com/kwangchun/honeybible/dto/LoginRequest.java
@@ -1,10 +1,14 @@
 package com.kwangchun.honeybible.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class LoginRequest {
+    @Schema(example = "홍길동")
     private String username;
+    @Schema(example = "2001")
     private String ttolae;
+    @Schema(example = "0606")
     private String lastPhoneNum;
 }
